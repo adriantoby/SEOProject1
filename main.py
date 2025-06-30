9c9c48e689084620dcf75576a5c75965e7acbfff
Adrian Moreno <morenoax7@gmail.com>
2025-06-30 21:49:24 +0000

    create workflow for project

---
+++ main.py
@@ -0,0 +1,34 @@
+'''
+
+Idea:
+    Ask user questions:
+        Exercise goal (strength training, endurance, …)
+        Experience level
+        Meal plan type (low carb, high protein, keto, …)
+        
+    Print workout plan and nutrition plan
+
+
+APIS: 
+    Workouts api: https://www.api-ninjas.com/api/exercises
+    Recipes api:https://spoonacular.com/food-api/apps
+    https://spoonacular.com/food-api/docs
+
+
+BONUS:
+	Access to gym 
+	Filter by equipment type
+
+
+WORKFLOW:
+    introduce the program -> print statement of what the project is
+    ask user questions
+        strength(1), endurance(2)
+        meal plan stuff
+    get JSONs from APIs
+    create database with JSON information
+    parse inputs into specific AI queries
+    display results in clean format
+    bonus features if time
+    
+'''
