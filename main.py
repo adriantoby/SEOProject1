7b54e0ed137f5e50e57350f89254484eb9312059
Adrian Moreno <morenoax7@gmail.com>
2025-07-02 20:44:52 +0000

    final fix for style

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from google.genai import types
 from dotenv import load_dotenv
 
 
+# collect user inputs
 print("Welcome to NutriFit: Your Convenient Fitness and Nutrition Planner!\n")
 
 experience = -1
@@ -51,9 +52,10 @@ while goal not in ["endurance", "strength", "bodybuilding"]:
 
 print()
 diet_type = -1
+text = "What diet do you prefer? (1-Vegetarian, 2-Low Carb, 3-High Protein)\n"
 while diet_type not in ["vegetarian", "low-carb", "high-protein"]:
     try:
-        diet_type = int(input("What diet do you prefer? (1-Vegetarian, 2-Low Carb, 3-High Protein)\n"))
+        diet_type = int(input(text))
     except ValueError:
         print("\nPlease enter a valid option.")
         continue
@@ -88,14 +90,17 @@ food_headers = {
     "x-api-key": food_api_key
 }
 
+
+# do GET requests
 exercise_base_url = "https://api.api-ninjas.com/v1/exercises"
 food_base_url = "https://api.spoonacular.com/recipes/"
 
-# do GET requests
-exercise_response = requests.get(exercise_base_url + f"?difficulty={experience}&type={workout_type}", headers=exercise_headers)
+exercise_response = requests.get(exercise_base_url + 
+    f"?difficulty={experience}&type={workout_type}", headers=exercise_headers)
 exercises = exercise_response.json()
 
-food_response = requests.get(food_base_url + food_base_url_extension, headers=food_headers)
+food_response = requests.get(food_base_url + 
+    food_base_url_extension, headers=food_headers)
 foods = food_response.json()
 if diet_type == "vegetarian":
     foods = foods["results"]
@@ -104,35 +109,43 @@ if diet_type == "vegetarian":
 # print(foods)
 
 
+# create and query database
 exercise_df = pd.DataFrame.from_dict(exercises)
 food_df = pd.DataFrame.from_dict(foods)
 
 engine = db.create_engine('sqlite:///health.db')
 
-exercise_df.to_sql('exercises', con=engine, if_exists='append', index=False)
-food_df.to_sql('foods', con=engine, if_exists='append', index=False)
+exercise_df.to_sql('exercises', con=engine, if_exists='replace', index=False)
+food_df.to_sql('foods', con=engine, if_exists='replace', index=False)
 
 with engine.connect() as connection:
-    exercise_query_result = connection.execute(db.text("SELECT * FROM exercises;")).fetchall()
-    food_query_result = connection.execute(db.text("SELECT * FROM foods;")).fetchall()
+    exercise_query_result = connection.execute(
+        db.text("SELECT * FROM exercises;")).fetchall()
+    food_query_result = connection.execute(
+        db.text("SELECT * FROM foods;")).fetchall()
 #    print(pd.DataFrame(query_result))
 
 
+# create and use GenAI API
 my_api_key = os.getenv('GENAI_API_KEY')
 
-# genai.api_key = my_api_key
-
-# Create an genAI client using the key from our environment variable
 client = genai.Client(
     api_key=my_api_key,
 )
 
-# Specify the model to use and the messages to send
 response = client.models.generate_content(
     model="gemini-2.5-flash",
     config=types.GenerateContentConfig(
-      system_instruction="You are a professional fitness and nutrition coach who knows most optimal fitness and nutrition plans for a user based on their background. You provide clear and concise plans and explanations with the most valuable information possible. You provide easy-to-read responses with no markdown syntax."),
-    contents=f"Look through {exercise_query_result} and {food_query_result} to create a workout and nutrition plan for {experience}s who want to focus on {goal} with a {diet_type}-focused diet. Keep both plans optimal and explained-well yet concise. Do not include IDs of the recipes in your answer.",
+      system_instruction="""You are a professional fitness and nutrition 
+       coach who knows most optimal fitness and nutrition plans for a user 
+       based on their background. You provide clear and concise plans and 
+       explanations with the most valuable information possible. You 
+       provide easy-to-read responses with no markdown syntax."""),
+    contents=f"""Look through {exercise_query_result} and 
+    {food_query_result} to create a workout and nutrition plan for 
+    {experience}s who want to focus on {goal} with a {diet_type}-focused 
+    diet. Keep both plans optimal and explained-well yet concise. Do not 
+    include IDs of the recipes in your answer.""",
 )
 
 print(response.text)
