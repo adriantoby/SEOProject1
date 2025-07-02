c61ab3007a3f714d5f4b4f6247de24227c4e55e8
Adrian Moreno <morenoax7@gmail.com>
2025-07-02 19:48:02 +0000

    remove commented workflow and resources

--- main.py
+++ main.py
@@ -6,42 +6,6 @@ from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 
-'''
-
-Idea:
-    Ask user questions:
-        Exercise goal (strength training, endurance, …)
-        Experience level
-        Meal plan type (low carb, high protein, keto, …)
-        
-    Print workout plan and nutrition plan
-
-
-APIS: 
-    Workouts api: https://www.api-ninjas.com/api/exercises
-    Recipes api:https://spoonacular.com/food-api/apps
-    https://spoonacular.com/food-api/docs
-
-
-BONUS:
-	Access to gym 
-	Filter by equipment type
-    Food allergies
-
-
-WORKFLOW:
-    introduce the program -> print statement of what the project is
-    ask user questions
-        strength(1), endurance(2)
-        meal plan stuff
-    set up authentication for APIS
-    get JSONs from APIs
-    create database with JSON information
-    parse inputs into specific AI queries
-    display results in clean format
-    bonus features if time
-
-'''
 
 print("Welcome to the Fitness and Nutrition Planner!\n")
 
