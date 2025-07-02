f58732335c2e283299855bf5f6318a50c68c7fea
Adrian Moreno <morenoax7@gmail.com>
2025-07-02 21:29:22 +0000

    hopefully final style check

--- main.py
+++ main.py
@@ -95,12 +95,14 @@ food_headers = {
 exercise_base_url = "https://api.api-ninjas.com/v1/exercises"
 food_base_url = "https://api.spoonacular.com/recipes/"
 
+exercise_base_url_extension = f"?difficulty={experience}&type={workout_type}"
 exercise_response = requests.get(exercise_base_url +
-f"?difficulty={experience}&type={workout_type}", headers=exercise_headers)
+                                 exercise_base_url_extension,
+                                 headers=exercise_headers)
 exercises = exercise_response.json()
 
-food_response = requests.get(food_base_url +
-food_base_url_extension, headers=food_headers)
+food_response = requests.get(food_base_url + food_base_url_extension,
+                             headers=food_headers)
 foods = food_response.json()
 if diet_type == "vegetarian":
     foods = foods["results"]
@@ -136,15 +138,15 @@ client = genai.Client(
 response = client.models.generate_content(
     model="gemini-2.5-flash",
     config=types.GenerateContentConfig(
-        system_instruction="""You are a professional fitness and nutrition 
-        coach who knows most optimal fitness and nutrition plans for a user 
-        based on their background. You provide clear and concise plans and 
-        explanations with the most valuable information possible. You 
+        system_instruction="""You are a professional fitness and nutrition
+        coach who knows most optimal fitness and nutrition plans for a user
+        based on their background. You provide clear and concise plans and
+        explanations with the most valuable information possible. You
         provide easy-to-read responses with no markdown syntax."""),
-    contents=f"""Look through {exercise_query_result} and 
-    {food_query_result} to create a workout and nutrition plan for 
-    {experience}s who want to focus on {goal} with a {diet_type}-focused 
-    diet. Keep both plans optimal and explained-well yet concise. Do not 
+    contents=f"""Look through {exercise_query_result} and
+    {food_query_result} to create a workout and nutrition plan for
+    {experience}s who want to focus on {goal} with a {diet_type}-focused
+    diet. Keep both plans optimal and explained-well yet concise. Do not
     include IDs of the recipes in your answer.""",
 )
 
