108a165753584526c792fd6f376badab0d3460a7
Adrian Moreno <morenoax7@gmail.com>
2025-07-03 03:35:47 +0000

    add unit tests

--- test_main.py
+++ test_main.py
@@ -6,4 +6,13 @@ import parser
 class TestMain(unittest.TestCase):
 
     def test_experience(self):
-        pass
+        self.assertEqual(parser.input_yoe(1), "beginner")
+        self.assertEqual(parser.input_yoe(20), "expert")
+
+    def test_goal(self):
+        correct = ("bodybuilding", "strength")
+        self.assertEqual(parser.input_goal(3), correct)
+
+    def test_diet(self):
+        correct = ("low-carb", "findByNutrients?minCarbs=15&maxCarbs=35")
+        self.assertEqual(parser.input_diet(2), correct)
